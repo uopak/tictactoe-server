9d28201796b06f62b83d0832c346986c27fb8d79
Junil Ko <junil@mocapot.com>
2025-03-04 11:51:37 +0900

    Initial commit

---
+++ app.js
@@ -0,0 +1,73 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mongodb = require('mongodb');       // 추가
+var MongoClient = mongodb.MongoClient;  // 추가
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// MongoDB 연결
+async function connectDB() {
+  var databaseURL = "mongodb://localhost:27017/tictactoe";
+
+  try {
+    const database = await MongoClient.connect(databaseURL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log("DB 연결 완료: " + databaseURL);
+
+    // 연결 종료 처리
+    process.on("SIGINT", async () => {
+      await database.close();
+      console.log("DB 연결 종료");
+      process.exit(0);
+    });
+  } catch (err) {
+    console.error("DB 연결 실패: " + err);
+    process.exit(1);
+  }
+}
+
+connectDB().catch(err => {
+  console.error("초기 DB 연결 실패: " + err);
+  process.exit(1);
+});
+
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
